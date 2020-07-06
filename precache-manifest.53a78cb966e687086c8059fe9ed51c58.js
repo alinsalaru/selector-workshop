e2e3a76f6a616ab25c4907647e3c4907663791a7
Alin Salaru <mplacintasalaru@deloittece.com>
2020-07-06 19:47:19 +0300

    Updates

---
+++ precache-manifest.53a78cb966e687086c8059fe9ed51c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48084a83f36fe891bd2d98ae7907fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edd24edb68f2c0d8bd99",
+    "url": "/static/css/main.5bc360d5.chunk.css"
+  },
+  {
+    "revision": "6450958e9b5e18b4854d",
+    "url": "/static/js/2.c8b7e9d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c8b7e9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd24edb68f2c0d8bd99",
+    "url": "/static/js/main.e98df885.chunk.js"
+  },
+  {
+    "revision": "765b4311e0961296930d",
+    "url": "/static/js/runtime-main.8232a255.js"
+  }
+]);
